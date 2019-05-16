5a390bd55b6a09188153eb205fb782ffca6eb525
geccsaa <geccsaa@hotmail.com>
2019-05-16 19:58:46 +0300

    Update xtreamcodes_1.0.60_2018.sh

--- xtreamcodes_1.0.60_2018.sh
+++ xtreamcodes_1.0.60_2018.sh
@@ -368,17 +368,9 @@ echo -e "${jeshile} └───────────────────
 echo " "
 #mkdir /var/www/html
 #cd /var/www/html #DESTINACIONI KRYESOR ADMIN - TEMPLATES ETC
-cd /home/ubuntu && wget http://raw.githubusercontent.com/marconimp/xtreamcodes_1.0.60/master/go.part01.rar
-wget http://raw.githubusercontent.com/marconimp/xtreamcodes_1.0.60/master/go.part02.rar
-wget http://raw.githubusercontent.com/marconimp/xtreamcodes_1.0.60/master/go.part03.rar
-wget http://raw.githubusercontent.com/marconimp/xtreamcodes_1.0.60/master/go.part04.rar
-wget http://raw.githubusercontent.com/marconimp/xtreamcodes_1.0.60/master/go.part05.rar
-wget http://raw.githubusercontent.com/marconimp/xtreamcodes_1.0.60/master/go.part06.rar
-wget http://raw.githubusercontent.com/marconimp/xtreamcodes_1.0.60/master/go.part07.rar
-wget http://raw.githubusercontent.com/marconimp/xtreamcodes_1.0.60/master/go.part08.rar
-wget http://raw.githubusercontent.com/marconimp/xtreamcodes_1.0.60/master/go.part09.rar
+cd /home/ubuntu && wget https://oturbutur.com/bakım/Xtream_Codes_v1.0.60_Nulled.zip
 sudo unrar x go.part01.rar
-cp -R /home/ubuntu/go/Xtream_Codes_v1.0.60_Nulled.zip  /var/www/html/
+cp -R /home/ubuntu/Xtream_Codes_v1.0.60_Nulled.zip  /var/www/html/
 cd /var/www/html && unzip Xtream_Codes_v1.0.60_Nulled.zip && cp /var/www/html/downloads/iptv_panel_pro.zip /tmp && chmod a+x /tmp/iptv_panel_pro.zip && cp /var/www/html/downloads/install_iptv_pro.php /root/ && cd /root && chmod a+x /root/install_iptv_pro.php && php install_iptv_pro.php
 chmod 775 /var/www/html/Xtream_Codes_v1.0.60_Nulled.zip
 rm /var/www/html/Xtream_Codes_v1.0.60_Nulled.zip
@@ -422,16 +414,6 @@ echo -e "${jeshile} │  Removing TEMP Files  │ \e[0m"
 echo -e "${jeshile} └───────────────────────┘ \e[0m"
 echo " "
 rm /home/ubuntu/xtreamcodes_1.0.60_novo.sh
-rm /home/ubuntu/go/
-rm /home/ubuntu/go.part01.rar
-rm /home/ubuntu/go.part02.rar
-rm /home/ubuntu/go.part03.rar
-rm /home/ubuntu/go.part04.rar
-rm /home/ubuntu/go.part05.rar
-rm /home/ubuntu/go.part06.rar
-rm /home/ubuntu/go.part07.rar
-rm /home/ubuntu/go.part08.rar
-rm /home/ubuntu/go.part09.rar
 rm /root/install_iptv_pro.php
 rm /root/xtreamcodes_1.0.60_novo.sh
 echo " "
